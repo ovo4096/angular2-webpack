bbdcd04d22cc71ef6d01eeda52c5cda87377f5d0
owwo <owwo@outlook.com>
2016-08-30 19:06:20 +0800

    start!

---
+++ src/vendor.ts
@@ -0,0 +1,6 @@
+// Polyfill Packages
+import 'core-js/client/shim';
+import 'zone.js/dist/zone';
+import 'reflect-metadata';
+
+import './assets/style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-    entry: './src/app/main.ts',
+    entry: {
+        vendor: './src/vendor.ts',
+        main: './src/main.ts'
+    },
     output: {
         path: "./dist/assets",
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         publicPath: "/assets/"
     },
     resolve: {
-
-    }
+        root: [
+            path.resolve('./src'),
+        ],
+        extensions: ['', '.js', '.ts', '.css']
+    },
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            loader: 'ts-loader'
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/index.html',
+            to: '..'
+        }])
+    ]
 }
